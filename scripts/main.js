baa20df6e3092955be3430169dc4642517445c06
Luis <luis.ruizi@cirvianum.cat>
2024-11-15 22:25:54 +0100

    Exercicii acabat

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,7 @@
-// Aquest codi ens ajuda a mostrar informació de les imatges de manera dinàmica
-
 const images = Array.from(document.querySelectorAll("img"));
 const infoContainers = Array.from(document.querySelectorAll(".image-info"));
 
-// Per saber la mida de la imatge en bytes, pots fer una petició fetch per
-// carregar la imatge i accedir a la seva longitud mitjançant el mètode
-// blob.size. Aquest procés et proporciona la mida de la imatge en bytes.
-
-// Més info sobre Blob --> https://es.javascript.info/blob
-
-async function getImageInfo(url) {
+async function getImageInfo(url, imgElement) {
   return new Promise(async (resolve, reject) => {
     const img = new Image();
     img.src = url;
@@ -17,15 +9,13 @@ async function getImageInfo(url) {
     img.onload = async () => {
       try {
         const response = await fetch(url);
-        // blob és un tipus de dades que representa un objecte de dades binàries. En el cas de les imatges
         const blob = await response.blob();
-        console.dir(blob);
         const format = url.split(".").pop();
         const dimensions = {
           width: img.width,
           height: img.height,
         };
-        const alt = img.alt;
+        const alt = imgElement.alt; 
         const size = blob.size;
 
         resolve({ format, dimensions, alt, size });
@@ -38,8 +28,8 @@ async function getImageInfo(url) {
   });
 }
 
-function displayImageInfo(url, container) {
-  getImageInfo(url)
+function displayImageInfo(url, container, imgElement) {
+  getImageInfo(url, imgElement)
     .then((info) => {
       const formatElement = document.createElement("p");
       formatElement.textContent = `Format: ${info.format}`;
@@ -61,8 +51,6 @@ function displayImageInfo(url, container) {
     .catch(console.error);
 }
 
-const container = document.querySelector("#image-info-container");
-
 images.forEach((img, i) => {
-  displayImageInfo(img.src, infoContainers[i]);
+  displayImageInfo(img.src, infoContainers[i], img); 
 });
